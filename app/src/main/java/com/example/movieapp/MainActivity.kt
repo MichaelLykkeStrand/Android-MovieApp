c8295362f2e954debfe60c6c19326ce3cf80243a
Michael Strand <32778224+MichaelLykkeStrand@users.noreply.github.com>
2021-03-23 10:42:12 +0100

    Working http request

--- app/src/main/java/com/example/movieapp/MainActivity.kt
+++ app/src/main/java/com/example/movieapp/MainActivity.kt
@@ -2,7 +2,14 @@ package com.example.movieapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
+import com.example.movieapp.api.ITheMovieDB
+import com.example.movieapp.api.TheMovieDBClient
+import com.example.movieapp.movie.MovieDetails
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,5 +22,19 @@ class MainActivity : AppCompatActivity() {
                 .add(R.id.root_layout, MovieDetailsFragment.newInstance(), "movieDetails")
                 .commit()
         }
+
+        val apiService : ITheMovieDB = TheMovieDBClient.getClient()
+        val call = apiService.getMovieDetails(527774)
+        call.enqueue(object : Callback<MovieDetails> {
+            override fun onFailure(call: Call<MovieDetails>?, t: Throwable?) {
+                Log.d("retrofit", "call failed")
+            }
+
+            override fun onResponse(call: Call<MovieDetails>?, response: Response<MovieDetails>?) {
+                Log.d("retrofit", "call success")
+            }
+
+        })
+
     }
 }
